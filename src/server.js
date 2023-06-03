5170596818d7d6d3e607043a7fe4c93bc873a0eb
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-06-03 20:15:27 +0900

    express, babel, nodemon 설정 및 초기 서버 구축

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express, { application } from "express";
+
+const PORT = 8000;
+const app = express();
+
+const handlePage = (req, res) => {
+    return res.send({ message : "This is home"})
+}
+app.get('/', handlePage)
+
+const handleLogin = (req, res) => {
+    return res.send({ message : 'This is Login Page'})
+}
+app.get('/login', handleLogin)
+
+const handleServer = () => console.log(`💫 Server listening on port http://localhost:${PORT} 🔥`)
+
+app.listen(PORT, handleServer)
